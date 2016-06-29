64bfef36ba5d8394261754b5ce63fe605bcae204
Chenhua Lu <luchenhua@icloud.com>
2016-06-29 15:51:07 +0800

    Demo done.

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,10 +1,25 @@
 // 头部JS
 function testJson() {
-    alert(dummy.city)
-    $('.dummy').append('<br />' + dummy.city)
-    // var url = '/user/get'
-    // $.getJSON(url, function(data) {
-    //     data = dummy.city
-    //     $('.dummy').append(data)
-    // })
+    var obj=window.location;
+    var path = obj.protocol + '//' + obj.host;
+
+    /* 通过Mock生成Dummy数据, 拦截ajax请求, 并模拟返回Json数据 */
+    Mock.mock(
+        path + '/user/get',
+        'get',
+        {
+            'name': '@name',
+            'age|1-100': 100,
+            'color': '@color'
+        }
+    );
+
+    var url = path + '/user/get';
+    var param = '';
+    var success = function (result) {
+        $('.dummy').append('<br/>' + result.name);
+        $('.dummy').append('<br/>' + result.age);
+        $('.dummy').append('<br/>' + result.color);
+    };
+    $.get(url, param, success, 'json');
 }

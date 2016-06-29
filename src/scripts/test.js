383a5a920fdf686fbed35762c328033bfc9aabc4
Chenhua Lu <luchenhua@icloud.com>
2016-06-29 11:07:23 +0800

    Backup

---
+++ src/scripts/test.js
@@ -0,0 +1,10 @@
+// 头部JS
+function testJson() {
+    alert(dummy.city)
+    $('.dummy').append('<br />' + dummy.city)
+    // var url = '/user/get'
+    // $.getJSON(url, function(data) {
+    //     data = dummy.city
+    //     $('.dummy').append(data)
+    // })
+}
